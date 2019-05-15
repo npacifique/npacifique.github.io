e34135380a8a93e1808761a73d9a23bb84cdaf26
Pacifique Ntahurunga <pacifique.niyibizi@gmail.com>
2019-05-15 16:06:17 -0400

    Update jsQuery.js
    updated ajax url call

--- jsQuery.js
+++ jsQuery.js
@@ -1,9 +1,21 @@
 //button event
 $("#button").click(function(){
+  
   //alert("button clicked!");
   $.ajax({
-    url:"http://192.168.1.4:5000/device"
-  })
+	url: 'http://192.168.1.4:5000/device',
+	type: 'post',
+	dataType: 'jsonp',
+	success: function(result){
+		alert(result);
+	},
+	error: function(result){
+		alert(result);
+	}
+});
+  
+  
+
 
 
   //$("#button").attr("disabled", false).delay(800);
